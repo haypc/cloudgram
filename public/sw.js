932fdeeeae54f2cae084411acf57d37732cea086
haypc <vardanpro7777@gmail.com>
2026-09-25 12:54:31 +0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cloudgram-shell-v1";
+const CACHE = "cloudgram-shell-v2";
 const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icon.svg"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
